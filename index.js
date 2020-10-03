74f2ee67ac42169c0331d20aca80c45ad0c42817
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-10-03 19:16:51 +0700

    Create API upload image

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 
 const authRoutes = require('./src/routes/auth');
 const blogRoutes = require('./src/routes/blog');
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+
 app.use(bodyParser.json()); //Yg di terima Type JSON
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- src/models/blog.js
+++ src/models/blog.js
@@ -11,6 +11,10 @@ const BlogPost = new Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
     author: {
         type: Object,
         required: true

b91f9ef46ea2d7fa31ddef934f7e69858a1a9e0b
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-10-14 20:21:12 +0700

    Create API get detail data

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -2,54 +2,73 @@ const { validationResult } = require('express-validator');
 const BlogPost = require('../models/blog');
 
 exports.createBlogPost = (req, res, next) => {
+	const errors = validationResult(req);
 
-  const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const err = new Error('Input value tidak sesuai');
+		err.errorStatus = 400;
+		err.data = errors.array();
+		throw err;
+	}
 
-  if (!errors.isEmpty()) {
-    const err = new Error('Input value tidak sesuai');
-    err.errorStatus = 400;
-    err.data = errors.array();
-    throw err;
-  }
+	if (!req.file) {
+		const err = new Error('Image Harus Di Upload');
+		err.errorStatus = 422;
+		throw err;
+	}
 
-  if (!req.file) {
-    const err = new Error('Image Harus Di Upload');
-    err.errorStatus = 422;
-    throw err;
-  }
+	const title = req.body.title;
+	const image = req.file.path;
+	const body = req.body.body;
 
-  const title = req.body.title;
-  const image = req.file.path;
-  const body = req.body.body;
+	const Posting = new BlogPost({
+		title: title,
+		body: body,
+		image: image,
+		author: { uid: 1, name: 'Randi Maulana Akbar' },
+	});
 
-  const Posting = new BlogPost({
-    title: title,
-    body: body,
-    image: image,
-    author: { uid: 1, name: 'Randi Maulana Akbar' }
-  });
-
-  Posting.save()
-    .then(result => {
-      res.status(201).json({
-        message: 'Create Blog Post Success',
-        data: result
-      });
-    })
-    .catch(error => {
-      console.log('error:', error);
-    });
-}
+	Posting.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Create Blog Post Success',
+				data: result,
+			});
+		})
+		.catch((error) => {
+			console.log('error:', error);
+		});
+};
 
 exports.getAllBlogPost = (req, res, next) => {
-  BlogPost.find()
-    .then(result => {
-      res.status(200).json({
-        message: 'Data blog post berhasil di panggil',
-        data: result
-      });
-    })
-    .catch(error => {
-      next(error)
-    });
-}
+	BlogPost.find()
+		.then((result) => {
+			res.status(200).json({
+				message: 'Data blog post berhasil di panggil',
+				data: result,
+			});
+		})
+		.catch((error) => {
+			next(error);
+		});
+};
+
+exports.getBlogPostById = (req, res, next) => {
+	const postId = req.params.postId;
+
+	BlogPost.findById(postId)
+		.then((result) => {
+			if (!result) {
+				const error = new Error('Blog post tidak ditemukan');
+				error.errorStatus = 404;
+				throw error;
+			}
+			res.status(200).json({
+				message: 'Data blog post berhasil di panggil',
+				data: result,
+			});
+		})
+		.catch((error) => {
+			next(error);
+		});
+};
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -3,15 +3,26 @@ const { body } = require('express-validator');
 
 const router = express.Router();
 
-const blogController = require('../controllers/blog')
+const blogController = require('../controllers/blog');
 
 //! [POST] : /v1/blog/post
-router.post('/post', [
-    body('title').isLength({ min: 5 }).withMessage('Input title yang anda masukan tidak sesuai'),
-    body('body').isLength({ min: 5 }).withMessage('Title title yang anda masukan tidak sesuai')],
-    blogController.createBlogPost);
+router.post(
+	'/post',
+	[
+		body('title')
+			.isLength({ min: 5 })
+			.withMessage('Input title yang anda masukan tidak sesuai'),
+		body('body')
+			.isLength({ min: 5 })
+			.withMessage('Title title yang anda masukan tidak sesuai'),
+	],
+	blogController.createBlogPost
+);
 
-//! [GET] : /v1/blog/post
+//! [GET] : /v1/blog/posts
 router.get('/posts', blogController.getAllBlogPost);
+
+//! [GET] : /v1/blog/posts/id
+router.get('/post/:postId', blogController.getBlogPostById);
 
 module.exports = router;
